9f6cff8e594c1da83ef546efd1a6e02dd3c00f9e
totny <24455370@qq.com>
2022-01-10 20:47:57 +0800

    将react\react-dom添加到externals

--- webpack.config.js
+++ webpack.config.js
@@ -28,5 +28,19 @@ module.exports = {
       title: 'T-UI',
       template: 'index.html'
     })
-  ]
+  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+  }
 }
